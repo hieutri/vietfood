dbc4464cc72d1f4e63b9b87c1a5902715e7139ff
hieutri <trihivu@gmail.com>
2022-07-02 18:29:03 +0700

    ok

--- js/general.js
+++ js/general.js
@@ -1,3 +1,13 @@
 document.getElementById("redirect-to-homepage").addEventListener("click", ()=>{
     window.location.href = "/html/homepage.html"
 })
+
+const shoppingCartContainer = document.getElementById("shopping-cart-container")
+
+document.getElementById("shopping-cart-icon").addEventListener("click", ()=> {
+    shoppingCartContainer.classList.add("shopping-cart-visible")
+})
+
+function turnOffCart(){
+    shoppingCartContainer.classList.remove("shopping-cart-visible")
+}
